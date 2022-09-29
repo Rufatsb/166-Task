5fc975c2261b3b7982d6ff64e57e1e9f82372d9e
Rufatsb <rufatsb@code.edu.az>
2022-09-30 00:13:40 +0400

    Project uploaded

---
+++ Js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    $('.slider-area').slick({
+        autoplay: false,
+        speed: 800,
+        arrows: false,
+        fade: true,
+        dots: true
+
+    });
+});
